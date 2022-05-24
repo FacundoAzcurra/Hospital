c62367a7eab03daf2654022a2997cd822cc5b20f
Facundo Azcurra Rech <fazcurra3@gmail.com>
2022-05-24 10:51:17 -0300

    json implemented

--- src/main/java/com/solvd/hospital/util/Json.java
+++ src/main/java/com/solvd/hospital/util/Json.java
@@ -1,10 +1,12 @@
 package com.solvd.hospital.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.solvd.hospital.bin.Medics;
+import com.solvd.hospital.bin.Nurses;
 import com.solvd.hospital.bin.Patients;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -20,17 +22,22 @@ public class Json {
         ObjectMapper mapper = new ObjectMapper();
 
         Patients patient = new Patients(1,true,"Tomas","Vino",1);
+        Patients patient2 = new Patients(2,false,"Clapa","Estruco",2);
+        Patients patient3 = new Patients(3,true,"Saul","Badman",3);
+        Patients patient4 = new Patients(4,false,"Ramon","Sanchopanza",4);
 
         try {
-            String json = mapper.writeValueAsString(patient);
-            System.out.println("ResultingJSONstring = " + json);
+            List<Patients> patients = new ArrayList<>();
+            patients.add(patient);
+            patients.add(patient2);
+            patients.add(patient3);
+            patients.add(patient4);
 
-            FileWriter fileWriter = new FileWriter(new File("src/main/resources/patient.json",json));
-
-            fileWriter.write(json);
-            fileWriter.flush();
-            fileWriter.close();
-
+            File file = new File("src/main/resources/parsedJSON.json");
+                if(!file.exists()){
+                    file.createNewFile();
+                }
+                mapper.writeValue(file,patients);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
